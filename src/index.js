9eb927e7ca8e9f9d3f93d157819a099bc6e8c920
Furkan Karadeniz <95278326+Suqbs@users.noreply.github.com>
2025-07-18 09:28:42 +0300

    Import the example data

--- src/index.js
+++ src/index.js
@@ -1,2 +1,30 @@
 import "./styles.css";
+import data from "./exampleData.json";
 
+console.log("Data:", data);
+
+// const exampleCity = "Ankara";
+
+// fetch(
+//   `https://weather-api-proxy-for-odin-project.vercel.app/api/proxy?search=${encodeURIComponent(exampleCity)}`,
+//   {
+//     method: "GET",
+//     headers: {},
+//     mode: "cors",
+//   },
+// )
+//   .then((response) => {
+//     console.log("Response:", response);
+
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! status: ${response.status}`);
+//     }
+
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log("Data", data);
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   });
